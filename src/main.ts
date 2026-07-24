3fb37e663c4e2f20982b48b6ee5dbab98a063a85
Gustavo <gustavobispocosta5521@gmail.com>
2026-07-23 22:56:23 -0300

    Criação do projeto com conexão com o frontendOS

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200', //URL do frontend
+    methods: 'GET, POST, PUT, PATCH, HEAD, DELETE', //m
+    credentials: true
+  });
+
+  await app.listen(3000);
+}
+bootstrap();
