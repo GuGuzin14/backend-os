34921ce42ed4fc66edd8933aa88a3da0f237641e
Gustavo <gustavobispocosta5521@gmail.com>
2026-08-18 22:14:41 -0300

    Apagando comentarios

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:4200', //URL do frontend
-    methods: 'GET, POST, PUT, PATCH, HEAD, DELETE', //m
+    methods: 'GET, POST, PUT, PATCH, HEAD, DELETE', 
     credentials: true
   });
 
